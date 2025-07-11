7e59a87c6a318420052b50ec1d235850ff18cca6
root <root@localhost>
2025-07-11 07:30:52 +0000

    WIP

--- src/main/java/com/nouhoun/springboot/jwt/integration/config/DatasourceConfig.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/config/DatasourceConfig.java
@@ -3,9 +3,7 @@ package com.nouhoun.springboot.jwt.integration.config;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -17,7 +15,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import jakarta.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-import java.beans.PropertyVetoException;
 
 /**
  * Created by nydiarra on 06/05/17.
@@ -28,19 +25,17 @@ import java.beans.PropertyVetoException;
 public class DatasourceConfig {
 
     @Bean
-    public DataSource datasource() throws PropertyVetoException {
+    public DataSource datasource() {
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase dataSource = builder
+        return builder
                 .setType(EmbeddedDatabaseType.H2)
-                .addScript("sql-scripts/schema.sql")
-                .addScript("sql-scripts/data.sql")
+                .addScript("CREATE TABLE IF NOT EXISTS USER (id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), role VARCHAR(255))")
+                .addScript("INSERT INTO USER (username, password, role) VALUES ('user', 'password', 'USER') ON DUPLICATE KEY UPDATE username = 'user'")
                 .build();
-
-        return dataSource;
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(@Qualifier("datasource") DataSource ds) throws PropertyVetoException{
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(@Qualifier("datasource") DataSource ds) {
         LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
         entityManagerFactory.setDataSource(ds);
         entityManagerFactory.setPackagesToScan(new String[]{"com.nouhoun.springboot.jwt.integration.domain"});
